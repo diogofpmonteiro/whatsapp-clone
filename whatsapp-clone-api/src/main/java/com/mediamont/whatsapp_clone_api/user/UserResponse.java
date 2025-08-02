8e4d54d279b2167fd362d948aede12beadbb6d0e
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-02 09:51:38 +0100

    implement user domain

---
+++ whatsapp-clone-api/src/main/java/com/mediamont/whatsapp_clone_api/user/UserResponse.java
@@ -0,0 +1,28 @@
+package com.mediamont.whatsapp_clone_api.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime lastSeen;
+    private boolean isOnline;
+
+
+
+}
