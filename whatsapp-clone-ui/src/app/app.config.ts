e4c82aedf4e05b408d559100d46ef462b4b6e0fa
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-03 10:42:36 +0100

    add and configure http interceptors

--- whatsapp-clone-ui/src/app/app.config.ts
+++ whatsapp-clone-ui/src/app/app.config.ts
@@ -2,13 +2,16 @@ import {ApplicationConfig, inject, provideAppInitializer} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {KeycloakService} from "./utils/keycloak/keycloak.service";
+import {keycloakHttpInterceptor} from "./utils/http/keycloak-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
     provideAppInitializer(() => {
       const initFn = ((key: KeycloakService) => {
         return () => key.init();
