70d818c6b16904623d87da91c9fec2164049d9cd
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-05 22:51:57 +0100

    implement sending messages with emojis

--- whatsapp-clone-ui/src/app/pages/main/main.component.ts
+++ whatsapp-clone-ui/src/app/pages/main/main.component.ts
@@ -5,11 +5,15 @@ import {ChatService} from "../../services/services/chat.service";
 import {KeycloakService} from "../../utils/keycloak/keycloak.service";
 import {MessageService} from "../../services/services/message.service";
 import {MessageResponse} from "../../services/models/message-response";
-import {DatePipe} from "@angular/common";
+import {DatePipe, NgOptimizedImage} from "@angular/common";
+import {PickerComponent} from "@ctrl/ngx-emoji-mart";
+import {FormsModule} from "@angular/forms";
+import {EmojiData} from "@ctrl/ngx-emoji-mart/ngx-emoji";
+import {MessageRequest} from "../../services/models/message-request";
 
 @Component({
   selector: 'app',
-  imports: [ChatListComponent, DatePipe],
+  imports: [ChatListComponent, DatePipe, PickerComponent, FormsModule, NgOptimizedImage],
   templateUrl: './main.component.html',
   standalone: true,
   styleUrl: './main.component.scss'
@@ -20,6 +24,8 @@ export class MainComponent implements OnInit {
   chats: Array<ChatResponse> = [];
   selectedChat: ChatResponse = {};
   chatMessages: MessageResponse[] = [];
+  showEmojis = false;
+  messageContent = "";
 
   constructor(
     private chatService: ChatService,
@@ -54,15 +60,13 @@ export class MainComponent implements OnInit {
     this.selectedChat = chatResponse;
     this.getAllChatMessages(chatResponse.id as string);
     this.setMessagesToSeen();
-    // this.selectedChat.unreadCount = 0;
+    this.selectedChat.unreadCount = 0;
   }
 
   private getAllChatMessages(chatId: string) {
     this.messageService
       .getMessages(
-        {
-          "chat-id": chatId
-        })
+        {"chat-id": chatId})
       .subscribe({
         next: messages => {
           this.chatMessages = messages;
@@ -71,10 +75,77 @@ export class MainComponent implements OnInit {
   }
 
   private setMessagesToSeen() {
-
+    this.messageService.setMessagesToSeen({
+      'chat-id': this.selectedChat.id as string,
+    }).subscribe({
+      next: message => {}
+    })
   }
 
   isSelfMessage(message: MessageResponse) {
     return message.senderId === this.keycloakService.userId;
+  }
+
+  uploadMedia(target: EventTarget | null) {
+
+  }
+
+  onSelectEmojis(selectedEmoji: any) {
+    const emoji: EmojiData = selectedEmoji.emoji;
+    this.messageContent += emoji.native;
+  }
+
+  keyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+
+  onClick() {
+    this.setMessagesToSeen();
+  }
+
+  sendMessage() {
+    if(this.messageContent) {
+      const messageRequest: MessageRequest = {
+        chatId: this.selectedChat.id,
+        senderId: this.getSenderId(),
+        recipientId: this.getRecipientId(),
+        content: this.messageContent,
+        messageType: 'TEXT'
+      };
+      this.messageService.saveMessage({
+        body: messageRequest,
+      }).subscribe({
+        next: () => {
+          const message: MessageResponse = {
+            senderId: this.getSenderId(),
+            recipientId: this.getRecipientId(),
+            content: this.messageContent,
+            type: 'TEXT',
+            state: 'SENT',
+            createdAt: new Date().toString(),
+          };
+          this.selectedChat.lastMessageDate = this.messageContent;
+          this.chatMessages.push(message);
+          this.messageContent = "";
+          this.showEmojis = false;
+        }
+      })
+    }
+  }
+
+  private getSenderId() {
+    if (this.selectedChat.senderId === this.keycloakService.userId) {
+      return this.selectedChat.senderId as string;
+    }
+    return this.selectedChat.recipientId as string;
+  }
+
+  private getRecipientId() {
+    if (this.selectedChat.senderId === this.keycloakService.userId) {
+      return this.selectedChat.recipientId as string;
+    }
+    return this.selectedChat.senderId as string;
   }
 }
